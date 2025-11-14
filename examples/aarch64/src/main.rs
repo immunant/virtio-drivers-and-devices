rcore-os/virtio-drivers

PCI: read full 64-bit size of 64-bit memory BARs
64-bit memory BARs not only have a 64-bit-wide address, but also a
64-bit-wide size. Change the bar_info() function to retrieve the upper
32 bits of the size and include it in the size calculation.

This modifies the return type of the BarInfo::memory_address_size()
function, so callers may need to modify their code to adapt to the new
64-bit size.

Fixes GitHub issue #177.

examples/aarch64/src/main.rs
@@ -418,6 +418,13 @@ fn allocate_bars(
             address_type, size, ..
         } = info
         {
+            // For now, only attempt to allocate 32-bit memory regions.
+            if size > u32::MAX.into() {
+                warn!("Skipping BAR {} with size {:#x}", index, size);
+                continue;
+            }
+            let size = size as u32;
+
             match address_type {
                 MemoryBarType::Width32 => {
                     if size > 0 {

immunant/virtio-drivers-and-devices

examples/aarch64/src/main.rs
@@ -430,6 +430,13 @@ fn allocate_bars(
             address_type, size, ..
         } = info
         {
+            // For now, only attempt to allocate 32-bit memory regions.
+            if size > u32::MAX.into() {
+                warn!("Skipping BAR {} with size {:#x}", bar_index, size);
+                continue;
+            }
+            let size = size as u32;
+
             match address_type {
                 MemoryBarType::Width32 => {
                     if size > 0 {